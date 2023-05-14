384f3b74a9287d1913a4557d321b37f30dbe3acb
Wendy Saules <wendysaules911230@gmail.com>
2023-05-14 17:13:58 -0500

    Add fake hoteles

---
+++ hola-telcel/experiencias/perfiles-prueba-3hotel.js
@@ -0,0 +1,318 @@
+//quintana roo
+{
+  data: [
+    //hoteles
+    //Hotel Moon Palace
+    {
+      id: 1,
+      name: "Hotel Moon Palace",
+      picture:
+        "https://www.sicssalab.net/contenido/lugares-experiencia/hoteles/1-moon-palace/avatar.png",
+      profileCover:
+        "https://www.sicssalab.net/contenido/lugares-experiencia/hoteles/1-moon-palace/portada.jpg",
+      members: "12k",
+      category: "Hotel",
+      type: "Publico",
+      content: [
+        {
+          id: 1,
+          picture:
+            "https://www.sicssalab.net/contenido/lugares-experiencia/hoteles/1-moon-palace/avatar.png",
+          createDate: "3min",
+          name: "Hotel Moon Palace",
+          likes: 367,
+          videos: [
+            "https://www.sicssalab.net/contenido/lugares-experiencia/hoteles/1-moon-palace/1.mp4",
+          ],
+          comments: [
+            {
+              id: 1,
+              userName: "Henrry Rockford",
+              picture:
+                "https://social-network.sicssa-lab.com/assets/images/avatars/5556512.png",
+              comment: "Un hotel muy lujoso!",
+              createDate: "3min",
+            },
+            {
+              id: 2,
+              userName: "Kim Portman",
+              picture:
+                "https://social-network.sicssa-lab.com/assets/images/avatars/beautiful-latin-woman-avatar-character-icon-free-vector.jpg",
+              comment: "Me encanta el servicio que brindan",
+              createDate: "7min",
+            },
+            {
+              id: 3,
+              userName: "Eduard Smith",
+              picture:
+                "https://social-network.sicssa-lab.com/assets/images/avatars/5556549.png",
+              comment: "De lujo",
+              createDate: "18min",
+            },
+            {
+              id: 4,
+              userName: "Miranda Jhonson",
+              picture:
+                "https://social-network.sicssa-lab.com/assets/images/avatars/beautiful-woman-red-hair-in-frame-circular-avatar-character-free-vector.webp",
+              comment: "el nivel de atención de este hotel es excelente",
+              createDate: "5hrs",
+            },
+          ],
+        },
+        {
+          id: 2,
+          picture:
+            "https://www.sicssalab.net/contenido/lugares-experiencia/hoteles/1-moon-palace/avatar.png",
+          createDate: "5hrs",
+          name: "Hotel Moon Palace",
+          likes: 482,
+          videos: [],
+          description:
+            "Ven y conoce las mejores instalaciones, todo para unas vacaciones magníficas: energía, lujo y posibilidades infinitas: comida gourmet, bebidas premium ilimitadas y mucho más.",
+          comments: [
+            {
+              id: 1,
+              userName: "Fivi Salem",
+              picture:
+                "https://social-network.sicssa-lab.com/assets/images/avatars/png-clipart-advertising-service-blog-internet-avatar-woman-face-black-hair.png",
+              comment: "Mis vacaiones fuerón excelentes, volveremos",
+              createDate: "7hrs",
+            },
+            {
+              id: 2,
+              userName: "Victor Plains",
+              picture:
+                "https://social-network.sicssa-lab.com/assets/images/avatars/placeholder.png",
+              comment: "Aquí no hace falta nada, comodidad 100%",
+              createDate: "9hrs",
+            },
+            {
+              id: 3,
+              userName: "Bob Thomson",
+              picture:
+                "https://social-network.sicssa-lab.com/assets/images/avatars/3641963.png",
+              comment: "De los mejores hoteles en los que eh estado.",
+              createDate: "11hrs",
+            },
+            {
+              id: 4,
+              userName: "Mike Smith",
+              picture:
+                "https://social-network.sicssa-lab.com/assets/images/avatars/images.png",
+              comment: "Me encanta este Hotel, es mi favorito.",
+              createDate: "1d",
+            },
+          ],
+        },
+      ],
+    },
+    //Hotel Le blanck
+    {
+      id: 1,
+      name: "Holel Le Blanc",
+      picture:
+        "https://www.sicssalab.net/contenido/lugares-experiencia/hoteles/1-moon-palace/avatar.png",
+      profileCover:
+        "https://www.sicssalab.net/contenido/lugares-experiencia/hoteles/1-moon-palace/portada.jpg",
+      members: "12k",
+      category: "Hotel",
+      type: "Publico",
+      content: [
+        {
+          id: 1,
+          picture:
+            "https://www.sicssalab.net/contenido/lugares-experiencia/hoteles/1-moon-palace/avatar.png",
+          createDate: "3min",
+          name: "Hotel Moon Palace",
+          likes: 367,
+          videos: [
+            "https://www.sicssalab.net/contenido/lugares-experiencia/hoteles/1-moon-palace/1.mp4",
+          ],
+          comments: [
+            {
+              id: 1,
+              userName: "Henrry Rockford",
+              picture:
+                "https://social-network.sicssa-lab.com/assets/images/avatars/5556512.png",
+              comment: "Un hotel muy lujoso!",
+              createDate: "3min",
+            },
+            {
+              id: 2,
+              userName: "Kim Portman",
+              picture:
+                "https://social-network.sicssa-lab.com/assets/images/avatars/beautiful-latin-woman-avatar-character-icon-free-vector.jpg",
+              comment: "Me encanta el servicio que brindan",
+              createDate: "7min",
+            },
+            {
+              id: 3,
+              userName: "Eduard Smith",
+              picture:
+                "https://social-network.sicssa-lab.com/assets/images/avatars/5556549.png",
+              comment: "De lujo",
+              createDate: "18min",
+            },
+            {
+              id: 4,
+              userName: "Miranda Jhonson",
+              picture:
+                "https://social-network.sicssa-lab.com/assets/images/avatars/beautiful-woman-red-hair-in-frame-circular-avatar-character-free-vector.webp",
+              comment: "el nivel de atención de este hotel es excelente",
+              createDate: "5hrs",
+            },
+          ],
+        },
+        {
+          id: 2,
+          picture:
+            "https://www.sicssalab.net/contenido/lugares-experiencia/hoteles/1-moon-palace/avatar.png",
+          createDate: "5hrs",
+          name: "Hotel Moon Palace",
+          likes: 482,
+          videos: [],
+          description:
+            "Ven y conoce las mejores instalaciones, todo para unas vacaciones magníficas: energía, lujo y posibilidades infinitas: comida gourmet, bebidas premium ilimitadas y mucho más.",
+          comments: [
+            {
+              id: 1,
+              userName: "Fivi Salem",
+              picture:
+                "https://social-network.sicssa-lab.com/assets/images/avatars/png-clipart-advertising-service-blog-internet-avatar-woman-face-black-hair.png",
+              comment: "Mis vacaiones fuerón excelentes, volveremos",
+              createDate: "7hrs",
+            },
+            {
+              id: 2,
+              userName: "Victor Plains",
+              picture:
+                "https://social-network.sicssa-lab.com/assets/images/avatars/placeholder.png",
+              comment: "Aquí no hace falta nada, comodidad 100%",
+              createDate: "9hrs",
+            },
+            {
+              id: 3,
+              userName: "Bob Thomson",
+              picture:
+                "https://social-network.sicssa-lab.com/assets/images/avatars/3641963.png",
+              comment: "De los mejores hoteles en los que eh estado.",
+              createDate: "11hrs",
+            },
+            {
+              id: 4,
+              userName: "Mike Smith",
+              picture:
+                "https://social-network.sicssa-lab.com/assets/images/avatars/images.png",
+              comment: "Me encanta este Hotel, es mi favorito.",
+              createDate: "1d",
+            },
+          ],
+        },
+      ],
+    },
+    // Hotel Nikelodeon
+    {
+      id: 1,
+      name: "Holel Le Blanc",
+      picture:
+        "https://www.sicssalab.net/contenido/lugares-experiencia/hoteles/1-moon-palace/avatar.png",
+      profileCover:
+        "https://www.sicssalab.net/contenido/lugares-experiencia/hoteles/1-moon-palace/portada.jpg",
+      members: "12k",
+      category: "Hotel",
+      type: "Publico",
+      content: [
+        {
+          id: 1,
+          picture:
+            "https://www.sicssalab.net/contenido/lugares-experiencia/hoteles/1-moon-palace/avatar.png",
+          createDate: "3min",
+          name: "Hotel Moon Palace",
+          likes: 367,
+          videos: [
+            "https://www.sicssalab.net/contenido/lugares-experiencia/hoteles/1-moon-palace/1.mp4",
+          ],
+          comments: [
+            {
+              id: 1,
+              userName: "Henrry Rockford",
+              picture:
+                "https://social-network.sicssa-lab.com/assets/images/avatars/5556512.png",
+              comment: "Un hotel muy lujoso!",
+              createDate: "3min",
+            },
+            {
+              id: 2,
+              userName: "Kim Portman",
+              picture:
+                "https://social-network.sicssa-lab.com/assets/images/avatars/beautiful-latin-woman-avatar-character-icon-free-vector.jpg",
+              comment: "Me encanta el servicio que brindan",
+              createDate: "7min",
+            },
+            {
+              id: 3,
+              userName: "Eduard Smith",
+              picture:
+                "https://social-network.sicssa-lab.com/assets/images/avatars/5556549.png",
+              comment: "De lujo",
+              createDate: "18min",
+            },
+            {
+              id: 4,
+              userName: "Miranda Jhonson",
+              picture:
+                "https://social-network.sicssa-lab.com/assets/images/avatars/beautiful-woman-red-hair-in-frame-circular-avatar-character-free-vector.webp",
+              comment: "el nivel de atención de este hotel es excelente",
+              createDate: "5hrs",
+            },
+          ],
+        },
+        {
+          id: 2,
+          picture:
+            "https://www.sicssalab.net/contenido/lugares-experiencia/hoteles/1-moon-palace/avatar.png",
+          createDate: "5hrs",
+          name: "Hotel Moon Palace",
+          likes: 482,
+          videos: [],
+          description:
+            "Ven y conoce las mejores instalaciones, todo para unas vacaciones magníficas: energía, lujo y posibilidades infinitas: comida gourmet, bebidas premium ilimitadas y mucho más.",
+          comments: [
+            {
+              id: 1,
+              userName: "Fivi Salem",
+              picture:
+                "https://social-network.sicssa-lab.com/assets/images/avatars/png-clipart-advertising-service-blog-internet-avatar-woman-face-black-hair.png",
+              comment: "Mis vacaiones fuerón excelentes, volveremos",
+              createDate: "7hrs",
+            },
+            {
+              id: 2,
+              userName: "Victor Plains",
+              picture:
+                "https://social-network.sicssa-lab.com/assets/images/avatars/placeholder.png",
+              comment: "Aquí no hace falta nada, comodidad 100%",
+              createDate: "9hrs",
+            },
+            {
+              id: 3,
+              userName: "Bob Thomson",
+              picture:
+                "https://social-network.sicssa-lab.com/assets/images/avatars/3641963.png",
+              comment: "De los mejores hoteles en los que eh estado.",
+              createDate: "11hrs",
+            },
+            {
+              id: 4,
+              userName: "Mike Smith",
+              picture:
+                "https://social-network.sicssa-lab.com/assets/images/avatars/images.png",
+              comment: "Me encanta este Hotel, es mi favorito.",
+              createDate: "1d",
+            },
+          ],
+        },
+      ],
+    },
+  ];
+}
