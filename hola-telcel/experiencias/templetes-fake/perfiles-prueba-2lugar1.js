958b91c35fe61ea721d93881ff1209b8823afd97
Wendy Saules <wendysaules911230@gmail.com>
2023-05-15 17:46:53 -0500

    re-organizacion lugares de experiencia perfiles

--- hola-telcel/experiencias/templetes-fake/perfiles-prueba-2lugar1.js
+++ hola-telcel/experiencias/templetes-fake/perfiles-prueba-2lugar1.js
@@ -4,75 +4,74 @@
     //lugares
         {
             "id": 1,
-            "name": "El arco del Tiempo",
-            "picture": "https://www.sicssalab.net/contenido/lugares-de-experiencia/3-chiapas/lugar-1/avatar.jpeg",
-            "profileCover": "https://www.sicssalab.net/contenido/lugares-de-experiencia/3-chiapas/lugar-1/perfil.jpeg",
+            "name": "El arco de los cabos",
+            "picture": "https://www.sicssalab.net/contenido/lugares-de-experiencia/13-baja-california/lugar-1/avatar.jpeg",
+            "profileCover": "https://www.sicssalab.net/contenido/lugares-de-experiencia/13-baja-california/lugar-1/perfil.jpeg",
             "members": "15k",
             "category": "Lugar1",
             "type": "Publico",
             "content": [
                 {
                     "id": 1,
-                    "picture": "https://www.sicssalab.net/contenido/lugares-de-experiencia/3-chiapas/lugar-1/avatar.jpeg",
+                    "picture": "https://www.sicssalab.net/contenido/lugares-de-experiencia/13-baja-california/lugar-1/avatar.jpeg",
                     "createDate": "3min",
-                    "name": "Quintana Roo",
+                    "name": "El arco de los cabos",
                     "likes": 367,
                     "videos": [
-                        "https://www.sicssalab.net/contenido/lugares-de-experiencia/3-chiapas/lugar-2/2.mp4"
+                        "https://www.sicssalab.net/contenido/lugares-de-experiencia/13-baja-california/lugar-1/2.mp4"
                     ],
                     "comments": [
                         {
                             "id": 1,
                             "userName": "Cloe Madison",
                             "picture": "https://social-network.sicssa-lab.com/assets/images/avatars/beautiful-woman-red-hair-in-frame-circular-avatar-character-free-vector.webp",
-                            "comment": "Espectacular!!!! Vale la pena las 3 horas de caminata.",
+                            "comment": "Que impresionante tanta belleza, el color del agua y la forma maravillosa del arco es lo que hace inigualable al destino.",
                             "createDate": "2seg"
                         },
                         {
                             "id": 2,
                             "userName": "Leanne Graham",
                             "picture": "https://social-network.sicssa-lab.com/assets/images/avatars/png-clipart-advertising-service-blog-internet-avatar-woman-face-black-hair.png",
-                            "comment": "Belleza natural que vale el esfuerzo visitar siempre y cuando respetando la selva y el espacio sin dañar flora y fauna",
+                            "comment": "Un lugar que hay que conocer, el arco y las playas cercanas son hermosas…",
                             "createDate": "1hr"
                         },
                         {
                             "id": 3,
                             "userName": "Nicholas Runolfsdottir",
                             "picture": "https://social-network.sicssa-lab.com/assets/images/avatars/5556512.png",
-                            "comment": "Lugar increíble para los amantes de la naturaleza y la aventura😊👍",
+                            "comment": "Maravilla natural imperdible, punto de poder y energía especial",
                             "createDate": "4hrs"
                         },
-                      
                     ]
                 },
                 {
                     "id": 2,
-                    "picture": "https://www.sicssalab.net/contenido/lugares-de-experiencia/3-chiapas/lugar-1/avatar.jpeg",
+                    "picture": "https://www.sicssalab.net/contenido/lugares-de-experiencia/13-baja-california/lugar-1/avatar.jpeg",
                     "createDate": "5hrs",
-                    "name": "Xcaret",
+                    "name": "El arco de los cabos",
                     "likes": 482,
                     "videos": [],
-                    "description": "Un arco de piedra natural de 180 metros de altura, uno de los más grandes del mundo en su género ubicado en el cañón del río La Venta de Cintalapa, el cañón es una barranca de 80 kilómetros de largo con paredes de hasta 400 metros de altura y angosturas en ocasiones de tan sólo 10 metros, en él se pueden encontrar cuevas y cascadas en sus paredes y miradores.",
+                    "description": "También conocido como el “Fin de la Tierra”, El Arco es la atracción turística más popular y el foco central de innumerables fotos de quienes visitan Los Cabos. Puedes llegar a este lugar en distintos tipos de embarcaciones y disfrutar de colonias de leones marinos, así como practicar actividades acuáticas como paddleboard.",
                     "comments": [
                         {
                             "id": 1,
                             "userName": "Cloe Weissnat",
                             "picture": "https://social-network.sicssa-lab.com/assets/images/avatars/beautiful-woman-red-hair-in-frame-circular-avatar-character-free-vector.webp",
-                            "comment": "👌 excelente experiencia,lla visita fue única y espectacular, volvería de nuevo a ir con ellos. ",
+                            "comment": "Hermoso lugar, es una parada obligatoria si vienes a los cabos !",
                             "createDate": "2seg"
                         },
                         {
                             "id": 2,
                             "userName": "Philip Thomson",
                             "picture": "https://social-network.sicssa-lab.com/assets/images/avatars/5556549.png",
-                            "comment": "Una de las mejores experiencias que he tenido en mis viajes, 100% recomendable, no hay duda que es la mejor empresa de turismo de aventura. ¡Este lugar es alucinante! Las fotos no le hacen justicia, es una belleza inigualable.",
+                            "comment": "Realmente lo mejor de los cabos es el arco que es algo majestuoso para conocer e impresionarse con esta maravilla que nos brinda la naturaleza",
                             "createDate": "1hr"
                         },
                         {
                             "id": 3,
                             "userName": "Henrry Marck",
                             "picture": "https://social-network.sicssa-lab.com/assets/images/avatars/5556512.png",
-                            "comment": "Es un viaje muy pesado pero que vale muchísimo la pena, toda una experiencia llena de aventura.😊👍",
+                            "comment": "Me encantaría conocerlo pronto 😎🤩😊👍",
                             "createDate": "4hrs"
                         }
                     ]
---
+++ hola-telcel/experiencias/templetes-fake/perfiles-prueba-2lugar2.js
@@ -0,0 +1,83 @@
+//quintana roo
+{
+ data: [    //lugares 2
+        {
+            "id": 1,
+            "name": "Playa Balandra",
+            "picture": "https://www.sicssalab.net/contenido/lugares-de-experiencia/13-baja-california/lugar-2/avatar.jpeg",
+            "profileCover": "https://www.sicssalab.net/contenido/lugares-de-experiencia/13-baja-california/lugar-2/perfil.jpeg",
+            "members": "15k",
+            "category": "Lugar2",
+            "type": "Publico",
+            "content": [
+                {
+                    "id": 1,
+                    "picture": "https://www.sicssalab.net/contenido/lugares-de-experiencia/13-baja-california/lugar-2/avatar.jpeg",
+                    "createDate": "3min",
+                    "name": "Ixtapa Zihuatanejo",
+                    "likes": 367,
+                    "videos": [
+                        "https://www.sicssalab.net/contenido/lugares-de-experiencia/13-baja-california/lugar-2/2.mp4"
+                    ],
+                    "comments": [
+                        {
+                            "id": 1,
+                            "userName": "Cloe Madison",
+                            "picture": "https://social-network.sicssa-lab.com/assets/images/avatars/beautiful-woman-red-hair-in-frame-circular-avatar-character-free-vector.webp",
+                            "comment": "De los lugares más espectaculares!",
+                            "createDate": "2seg"
+                        },
+                        {
+                            "id": 2,
+                            "userName": "Leanne Graham",
+                            "picture": "https://social-network.sicssa-lab.com/assets/images/avatars/png-clipart-advertising-service-blog-internet-avatar-woman-face-black-hair.png",
+                            "comment": "Es un lugar de una belleza indescriptible. 💕🤩 Este lugar es mágico..",
+                            "createDate": "1hr"
+                        },
+                        {
+                            "id": 3,
+                            "userName": "Nicholas Runolfsdottir",
+                            "picture": "https://social-network.sicssa-lab.com/assets/images/avatars/5556512.png",
+                            "comment": "Tiene una maravillosa vista. Si el agua es única 😊👌",
+                            "createDate": "4hrs"
+                        },
+                       
+                    ]
+                },
+                {
+                    "id": 2,
+                    "picture": "https://www.sicssalab.net/contenido/lugares-de-experiencia/13-baja-california/lugar-2/avatar.jpeg",
+                    "createDate": "5hrs",
+                    "name": "Ixtapa Zihuatanejo",
+                    "likes": 482,
+                    "videos": [],
+                    "description": "A unos 20 minutos en coche del centro de La Paz se encuentra playa Balandra. Ha sido nombrada en varias ocasiones como la mejor playa de México. ¡Y no es poco teniendo una fuerte competencia!",
+                    "comments": [
+                        {
+                            "id": 1,
+                            "userName": "Cloe Weissnat",
+                            "picture": "https://social-network.sicssa-lab.com/assets/images/avatars/beautiful-woman-red-hair-in-frame-circular-avatar-character-free-vector.webp",
+                            "comment": "¡Es el paraíso! Una playa hermosa que hay que cuidar mucho cuando la visitemos!",
+                            "createDate": "2seg"
+                        },
+                        {
+                            "id": 2,
+                            "userName": "Philip Thomson",
+                            "picture": "https://social-network.sicssa-lab.com/assets/images/avatars/5556549.png",
+                            "comment": "Excelente lugar para aprender, conocer la cultura y pasar un rato muy agradable con la naturaleza 😉😊❤️",
+                            "createDate": "1hr"
+                        },
+                        {
+                            "id": 3,
+                            "userName": "Henrry Marck",
+                            "picture": "https://social-network.sicssa-lab.com/assets/images/avatars/5556512.png",
+                            "comment": "Para los amantes de la naturaleza una vista impresionante",
+                            "createDate": "4hrs"
+                        }
+                    ]
+                }
+            ]
+        }
+    ]
+}
+
